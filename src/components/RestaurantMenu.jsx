57110ab6e8cdba5f3203e6e4e91c9b14799148c5
Ritikesh-Raj <ritikesh.raj04@gmail.com>
2023-07-25 20:44:25 +0530

    extended

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -2,11 +2,14 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu"
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
+
+  const [showIndex, setShowIndex] = useState(0);
 
   if (resInfo === null) return <Shimmer />;
 
@@ -23,18 +26,25 @@ const RestaurantMenu = () => {
     (c) =>
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-  )
+  );
 
   return (
     <div className="text-center">
-      <h1 className="font-bold text-2xl my-6 text-center">{name}</h1>
-      <p className="font-bold text-center my-2 text-xl">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      {/* categories accordian */}
-      {categories.map((category) => 
-      <RestaurantCategory key={category?.card?.card.title} 
-      data={category?.card?.card}/>)}
+      {/* categories accordions */}
+      {categories.map((category, index) => (
+        // controlled component
+        <RestaurantCategory
+          key={category?.card?.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex={() => setShowIndex(index)}
+        
+        />
+      ))}
     </div>
   );
 };
